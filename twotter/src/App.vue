c118158a6557106e88abdea0d410274123c00ca4
Madhav Gupta <madhavsvg@gmail.com>
2021-01-17 22:58:58 +0100

    MAD: created the base of vuejs 3. Tried methods, computed, templates and click event.

---
+++ twotter/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    @{{ user.userName }} - {{fullName}}
+    <strong>Followers: </strong> {{followers}}
+    <button @click="followUser">
+      Follow
+    </button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      followers:0,
+      user:{
+        id: 1,
+        userName: 'madhavsvg',
+        firstName: 'Madhav',
+        lastName: 'Gupta',
+        email: 'madhavsvg@gmail.com',
+        isAdmin: true
+      }
+    }
+  },
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`
+    }
+  },
+  methods: {
+    followUser(){
+      this.followers++;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+}
+</style>
